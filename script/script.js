f8ace3ac272f93e0c58e607e07ab3d764f88647f
hridoy479 <hridoymolla479@gmail.com>
2024-07-13 09:14:14 +0600

    scriptupdate

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const offbtn = document.getElementById('offbtn');
 const sp1 = document.getElementById('sp1');
 const sp2 = document.getElementById('sp2');
 const sp3 = document.getElementById('sp3');
-const sound = new Audio('/img/fansound.mp3');
+const sound = new Audio('./img/fansound.mp3');
 const fanimg = document.getElementById('fanimg');
 const video = document.getElementById('myVideo');
 
